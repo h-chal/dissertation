7069511f26637ef9555b228b6da61603675dcfff
Harper Challinor <h@h-chal.com>
2025-02-18 19:39:31 +0000

    feat: make in CoreMark script

--- TooobaWrapper/try_coremark.sh
+++ TooobaWrapper/try_coremark.sh
@@ -1,11 +1,11 @@
-echo "Please ensure you have recompiled Toooba if any changes are made to it or to the predictor in use."
-
 # TODO
 # Configure which predictor used. ProcConfig.bsv. try putting it in a less central file for faster make
 # Recompile when config changed or predictor source changed
 
 
 cd TooobaWrapper
+
+make -C Toooba/builds/RV64ACDFIMSU_Toooba_bluesim -j$(nproc) compile simulator > /dev/null
 
 ln -fs coremark_gcc.hex Mem.hex
 
