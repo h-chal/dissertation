6336d98e48b76e335e09f5bb3db785823ce2c009
Harper Challinor <h@h-chal.com>
2025-02-18 19:10:46 +0000

    feat: improve CoreMark output

--- TooobaWrapper/try_coremark.sh
+++ TooobaWrapper/try_coremark.sh
@@ -3,8 +3,6 @@ echo "Please ensure you have recompiled Toooba if any changes are made to it or 
 # TODO
 # Configure which predictor used. ProcConfig.bsv. try putting it in a less central file for faster make
 # Recompile when config changed or predictor source changed
-# See if LOG_DEADLOCK_CYCLES can be used. I dont think its implemented since theres a todo for CHECK_DEADLOCK in make
-# Get results out
 
 
 cd TooobaWrapper
@@ -12,3 +10,5 @@ cd TooobaWrapper
 ln -fs coremark_gcc.hex Mem.hex
 
 Toooba/builds/RV64ACDFIMSU_Toooba_bluesim/exe_HW_sim > /tmp/toooba_output.txt
+
+tail --lines=1 /tmp/toooba_output.txt | xargs
